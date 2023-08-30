cb1d38f551bf28b8e6e8c3b5482489d03298e20b
Denis Bezykornov <archidevil52@gmail.com>
2023-08-30 20:00:08 +0300

    Initial app bootstrap

---
+++ web/src/App.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<template>
+  <h1 class="text-slate-800">Hello, World!</h1>
+</template>
