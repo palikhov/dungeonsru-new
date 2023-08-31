85e6367234fb01a38692bc19f8860c67d6b3e1c3
Denis Bezykornov <archidevil52@gmail.com>
2023-09-01 02:14:59 +0300

    Add main page styles

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,vue}"],
+  content: ["./src/**/*.{html,js,ts,vue}", "./index.html"],
   theme: {
     extend: {
       container: {
