6e8a8335f40b3308e42939325af6dd28eaeb972b
Denis Bezykornov <archidevil52@gmail.com>
2023-09-02 17:00:19 +0300

    Add pinia

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import { defaults } from 'mande'
+import {createApp} from 'vue'
+import {defaults} from 'mande'
 
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import Posts from './views/Posts.vue'
 import Books from './views/Books.vue'
+import {createPinia} from 'pinia'
 
 const token = __DIRECTUS_TOKEN__
 defaults.headers.Authorization = `Bearer ${token}`
@@ -13,11 +14,14 @@ defaults.headers.Authorization = `Bearer ${token}`
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Posts },
-    { path: '/books', component: Books },
+    {path: '/', component: Posts},
+    {path: '/books', component: Books},
   ],
 })
 
+const pinia = createPinia()
 const app = createApp(App)
+
 app.use(router)
+app.use(pinia)
 app.mount('#app')
