0470fb2863fe10599f8368d74b9ca94a3972301d
Denis Bezykornov <archidevil52@gmail.com>
2023-09-02 17:01:57 +0300

    Add mock server plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mockServer from 'vite-plugin-mock-server'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      ...mockServer({logLevel: 'info'}),
+      apply: 'serve',
+    },
+  ],
 })
